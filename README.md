db014712c9c47893e8f029b52228521ac2bf292c
grcnola <grace.nolanda@si.ukdw.ac.id>
2022-11-21 19:41:11 +0700

    update

---
+++ README.md
@@ -0,0 +1,11 @@
+# ArgaTravel
+Aplikasi travel agent management yang dibangun menggunakan bahasa PHP dengan framework CodeIgniter 3.
+
+Untuk Login sebagai user,
+pergi ke http://localhost/argatravel
+ 1. username : argahutama, password : 12345678
+  
+Untuk Login sebagai admin/supervisor,
+pergi ke http://localhost/argatravel/backend
+ 1. username : admin, password : admin
+ 2. username : supervisor, password : admin
---
+++ application/views/frontend/include/base_footer.php
@@ -0,0 +1,40 @@
+			<footer class="footer-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-7  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4 class="text-white">Tiket BUS</h4>
+								<p>
+									Arga Travel adalah jasa pemesanan tiket bis secara online. Telah dipercaya lebih dari 1 juta pelanggan secara nasional. Arga Travel menawarkan pemesanan tiket bis melalui website, iOS dan aplikasi android untuk rute-rute utama berbagai kota di Indonesia.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4 class="text-white">Contact Us</h4>
+								<p>
+									Arga Hutama
+								</p>
+								<p class="number">
+									+62 89 509 509 444
+								</p>
+								<div class="footer-bottom d-flex justify-content-between align-items-center flex-wrap">
+								<div class="footer-social d-flex align-items-left">
+									<a href="https://facebook.com/argahut"><i class="fa fa-facebook"></i></a>
+									<a href="https://instagram.com/argahutama"><i class="fa fa-instagram"></i></a>
+						</div>
+							</div>
+						</div>											
+					</div>
+					<div class="footer-bottom d-flex justify-content-between align-items-center flex-wrap">
+            			<p class="footer-text m-0">Copyright &copy;2020 Arga Hutama</p>
+					</div>
+				</div>
+			</footer>
+			<div class="preloader">
+			<div class="loading">
+				<img src="<?php echo base_url('assets/frontend/img/preloader.gif') ?>" width="80">
+				<p>Harap Tunggu</p>
+			</div>
+		</div>	
+			<!-- End footer Area -->
